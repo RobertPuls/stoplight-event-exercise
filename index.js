1d61979b1e9bb410a5798760825c83424d81aab9
Robert Puls <rpu1s@sbcglobal.net>
2017-05-06 19:32:27 -0600

    completed

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 (function() {
-  'use strict';
-
+  "use strict";
   // YOUR CODE HERE
+  let buttons = document.getElementsByClassName("button");
+  let lights = document.getElementsByClassName("bulb");
+  for(let i = 0; i < buttons.length; i++){
+     buttons[i].addEventListener("mouseenter", function () {
+       console.log("Entered" , buttons[i].innerText, "button");
+     });
+     buttons[i].addEventListener("mouseleave", function () {
+       console.log("Left" , buttons[i].innerText, "button");
+     });
+     buttons[i].addEventListener("click", function(){
+       let myStr = buttons[i].innerText.toLowerCase();
+       lights[i].classList.toggle(myStr);
+       (lights[i].classList.contains(myStr)) ? console.log(myStr, "bulb is on"):console.log(myStr, "bulb is off");
+     });
+}
 })();
